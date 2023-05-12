b29556f5573e488af4bf7e068631f74b5ca7e6cc
Tufan Ulukaya <119865482+CodeTock@users.noreply.github.com>
2023-05-12 21:17:28 +0300

    Add files via upload

---
+++ Amazing Ultra deluxe Premium Plus Web Page/index.js
@@ -0,0 +1,13 @@
+const navbarEl = document.querySelector(".navbar");
+const bottomContainerEl = document.querySelector(".bot-container");
+
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY >
+    bottomContainerEl.offsetTop - navbarEl.offsetHeight - 50
+  ) {
+    navbarEl.classList.add("active");
+  } else {
+    navbarEl.classList.remove("active");
+  }
+});
